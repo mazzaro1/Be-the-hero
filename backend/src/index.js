4b16857073a29fbe736b099f5368c67efcebce23
mazzaro1 <gustamazzaro@gmail.com>
2020-03-24 20:07:28 -0300

    Construindo a base da aplicação (back-end)
    Construindo a base da aplicação (back-end)

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
+
+/*
+*Métodos HTPP:
+*
+*GET: Buscar uma informação do back-end
+*POST: Criar uma informação no back-end
+*Put: Modificar uma informação no back-end
+*DELETE: deletetar uma informação no back-end
+*/
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query params: parametros nomeados enviados na rota apos "?"(filtros, paginação).
+ * Route Params: parametros utilizados para identificar recursos
+ * Request body : Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+ /**
+  * SQL: MySQL, SQLite, PostgreeSQL, Oracle, Microsoft SQL server;
+  * NoSQL: MongoDB, CouchDB, etc.
+  */
+
+  /**
+   * Driver: SELECT * FROM users
+   * Query Builder: table('users').select('*').where()
+   */
+
---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController')
+
+const IncidentController = require('./controllers/IncidentController')
+
+const ProfileController = require('./controllers/ProfileController')
+
+const routes = express.Router();
+
+
+routes.get('/profile',ProfileController.index)
+routes.get('/ongs',OngController.index);
+routes.post('/ongs',OngController.create);
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id',IncidentController.delete)
+             
+ module.exports = routes;
